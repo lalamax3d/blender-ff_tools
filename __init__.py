87be61a1b8879a512497fd4b4679fb330cb79acc
Haseeb Ahmed <lalamax3d@gmail.com>
2021-02-10 13:55:45 +0500

    prism render paths feature

--- __init__.py
+++ __init__.py
@@ -29,6 +29,8 @@ from . ff_model import SelectHalfMesh_OT_Operator, ReMirror_OT_Operator, FindMis
 from . ff_facecap import FfFaceCapPropGrp
 from . ff_rig import UseSingleSideControls_OT_Operator, SelectOneSidedObjects_OT_Operator, SetEulerRotations_OT_Operator, SetQuatRotations_OT_Operator , exportDriversToJson_OT_Operator, importDriversFromJson_OT_Operator, FF_PT_Rig
 from . ff_anim import CopyIklegs_OT_Operator, CopyIkArms_OT_Operator, KeySelectionOp_OT_Operator, KeyDeletionOp_OT_Operator, FF_PT_Anim
+from . ff_rend import setupPrismOutput_OT_Operator, FF_PT_Rend
+
 
 from . ff_sk import SkZeroAll_OT_Operator,SkAnimateAll_OT_Operator,SkBindToBone_OT_Operator
 from . ff_facecap import ReadFaceCapJson_OT_Operator, SetupFcBoneProps_OT_Operator, SetupFcSingleDriver_OT_Operator, SetupFcDrivers_OT_Operator
@@ -37,6 +39,7 @@ bpy.types.Scene.ff_general = bpy.props.BoolProperty(default=False)
 bpy.types.Scene.ff_rigging = bpy.props.BoolProperty(default=False)
 bpy.types.Scene.ff_anim = bpy.props.BoolProperty(default=False)
 bpy.types.Scene.ff_facecap = bpy.props.BoolProperty(default=False)
+bpy.types.Scene.ff_rend = bpy.props.BoolProperty(default=False)
 
 # bpy.types.Scene.ff_skFilter = bpy.props.StringProperty(default='search shape')
 bpy.types.Scene.ff_anim_kr = bpy.props.IntProperty(default=4,min=2,max=9,step=1)
@@ -65,6 +68,7 @@ class FF_PT_Panel(bpy.types.Panel):
         row.prop(bpy.context.scene,"ff_general",text="Misc",icon="EDITMODE_HLT")
         row.prop(bpy.context.scene,"ff_rigging",text="Rigg",icon="ARMATURE_DATA")
         row.prop(bpy.context.scene,"ff_anim",text="Anim",icon="POSE_HLT")
+        row.prop(bpy.context.scene,"ff_rend",text="Rend",icon="IMAGE_PLANE")
 
 classes = (
         SelectHalfMesh_OT_Operator, ReMirror_OT_Operator, FindMissingFiles_OT_Operator, FixDuplicateMaterials_OT_Operator,
@@ -74,6 +78,7 @@ classes = (
         CopyIkArms_OT_Operator, CopyIklegs_OT_Operator,
         KeySelectionOp_OT_Operator,KeyDeletionOp_OT_Operator,
         ReadFaceCapJson_OT_Operator, SetupFcBoneProps_OT_Operator,SetupFcSingleDriver_OT_Operator,SetupFcDrivers_OT_Operator,
-        FF_PT_Panel, FF_PT_Model, FF_PT_Rig, FF_PT_Anim)
+        setupPrismOutput_OT_Operator, 
+        FF_PT_Panel, FF_PT_Model, FF_PT_Rig, FF_PT_Anim, FF_PT_Rend)
 
 register,unregister = bpy.utils.register_classes_factory(classes)
---
+++ ff_rend.py
@@ -0,0 +1,157 @@
+import bpy
+from bpy import context as context
+
+from pathlib import Path
+
+import PrismCore
+
+global pcore
+pcore = PrismCore.PrismCore(app="Blender")
+
+
+def isPrismFile(context):
+    ''' 
+        this function will check if valid shot is opened from Current Prism project and return yes or not
+    '''
+    pcore.projectPath
+    pcore.scenePath # points to workflow dir
+    pcore.shotPath # points to shots folder
+    pcore.fileInPipeline(bpy.data.filepath)
+    bfp = Path(bpy.data.filepath) 
+    #filename = bpy.path.basename(bpy.data.filepath)
+    filename = bfp.name
+    filepath = bfp.parent
+    shotdir = bfp.parent.parent.parent.parent
+    if shotdir.is_dir():
+        rendDir = shotdir.joinpath("Rendering","3dRender")
+    # now get file version string
+    verInfo = filename.split(pcore.filenameSeparator)[4] # .sequenceSeparato
+    verFormat = pcore.versionFormat
+    
+    # create / output dir exists
+    print ("RENDERING DIR EXPECTED PATH:", rendDir)
+    print ("RENDERING DIR FOUND:", rendDir.exists())
+    renderer = context.scene.render.engine.split('_')[1]
+    rendDir = rendDir.joinpath(renderer)
+    rendDir.mkdir(parents=True, exist_ok=True) # will create EEVEE or CYCLES Directory
+
+    # set relative path in output (main area)
+    #fo = C.scene.render.filepath
+    #bpy.path.abspath, to replace os.path.abspath
+    #bpy.path.relpath, to replace os.path.relpath
+    context.scene.render.filepath = rendDir.as_posix() + '/'+verInfo+'/beauty/image_'
+
+    # remove all fileoutput nodes in comp area
+    outputNodes = [x for x in context.scene.node_tree.nodes if x.type == 'OUTPUT_FILE']
+    print ('OUTPUT NODES COUNT:', len(outputNodes))
+    for node in outputNodes:
+        context.scene.node_tree.nodes.remove(node)
+    # setup new output comp nodes in comp area
+    #n = bpy.context.scene.node_tree.nodes.new(type='CompositorNodeOutputFile')
+    # set new node location >> n.location = ((50,50))
+    # Get Render Layer
+    rendLyrNodes = [x for x in context.scene.node_tree.nodes if x.type == 'R_LAYERS']
+    if len(rendLyrNodes) ==1:
+        rendLyrNode = rendLyrNodes[0]
+    else:
+        #create new render layer node
+        pass
+    # get output passes list from node or blender
+    #rendLyrNode.outputs # all possible passes here (todo)
+    tree = context.scene.node_tree
+    print ("TREE:", tree)
+    np = rendLyrNode.viewLocation
+    iteration = 1
+    renderPasses = {
+        "DiffCol": context.scene.view_layers[0].use_pass_diffuse_color,
+        "DiffDir": context.scene.view_layers[0].use_pass_diffuse_direct,
+        "GlossCol": context.scene.view_layers[0].use_pass_glossy_color,
+        "GlossDir": context.scene.view_layers[0].use_pass_glossy_direct,
+        "Shadow": context.scene.view_layers[0].use_pass_shadow,
+        "AO": context.scene.view_layers[0].use_pass_ambient_occlusion,
+        "Mist": context.scene.view_layers[0].use_pass_mist,
+        "Emit": context.scene.view_layers[0].use_pass_emit,
+        "Normal": context.scene.view_layers[0].use_pass_normal,
+        "Depth": context.scene.view_layers[0].use_pass_z
+    }
+    #if context.scene.view_layers[0].use_pass_diffuse_color:
+    #    n = context.scene.node_tree.nodes.new(type='CompositorNodeOutputFile')
+    #    tree.links.new(rendLyrNode.outputs['DiffCol'], n.inputs[0])
+    for each in renderPasses:
+        if renderPasses[each]:
+            n = context.scene.node_tree.nodes.new(type='CompositorNodeOutputFile')
+            tree.links.new(rendLyrNode.outputs[each], n.inputs[0])
+            n.base_path =  rendDir.as_posix() + '/'+verInfo+'/'+each
+            npx = np.x + 800
+            npy = np.y - (iteration * 100)
+            n.location = ((npx,npy))
+            iteration = iteration + 1
+    # create links
+    # C.scene.node_tree.links.new('','')
+    # fix zdepth
+    
+    # make sure compositing is ticked 
+
+def useCompositing_toggle():
+    C.scene.render.use_compositing = not C.scene.render.use_compositing
+ 
+def getRenderInfo():
+    fo = C.scene.render.filepath
+    #bpy.path.abspath, to replace os.path.abspath
+    #bpy.path.relpath, to replace os.path.relpath
+
+     
+def setRP_FileOuts(type='png'):
+    pass
+def updateRP_FileOuts(type='png'):
+    for node in C.scene.node_tree.nodes:
+        if node.type == 'OUTPUT_FILE':
+            no = node.base_path
+
+
+
+# OPERATORS HERE
+
+
+class setupPrismOutput_OT_Operator (bpy.types.Operator):
+    '''setup local png render'''
+    bl_idname = "ffrend.setup_prism_output"
+    bl_label = "ffrend_setupPrismOutput"
+    bl_options =  {"REGISTER","UNDO"}
+    '''
+    @classmethod
+    def poll(cls,context):
+        if context.area.type=='VIEW_3D':
+            if ((context.object) and context.object.type =="ARMATURE" and ('rig_id' in context.object.data)):
+                return (1)
+        else:
+            return(0)
+    '''
+    def execute(self, context):
+        isPrismFile(context)
+        self.report({'INFO'}, "Done.")
+        return{"FINISHED"}
+class FfPollRend():
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    @classmethod
+    def poll(cls, context):
+        return(context.scene.ff_rend == True)
+class FF_PT_Rend(FfPollRend, bpy.types.Panel):
+    bl_idname = "FF_PT_Rend"
+    bl_label = "Rendering"
+    bl_category = "FF_Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    def draw(self, context):
+        active_obj = context.active_object
+        layout = self.layout
+        # new stuff
+        box = layout.box()
+        col = box.column(align = True)
+        col.label(text='PRISM ASSIST')
+        row = col.row(align=True)
+        # row.prop(s,'fc_boneProps',text='Props')
+        #row4 = col3.row(align = True)
+        row.operator("ffrend.setup_prism_output", text="Setup Prism Render (PNG)")
